9d94ff8653b10e315db15602b6341c682d859bcb
bugsounet <bugsounet@bugsounet.fr>
2022-07-24 16:54:35 +0200

    add Greek translation

--- EXT-Internet.js
+++ EXT-Internet.js
@@ -45,7 +45,8 @@ Module.register("EXT-Internet", {
       es: "translations/es.json",
       nl: "translations/nl.json",
       pt: "translations/pt.json",
-      ko: "translations/ko.json"
+      ko: "translations/ko.json",
+      el: "translations/el.json"
     }
   },
 
